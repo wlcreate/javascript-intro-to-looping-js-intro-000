5d19b774dff2b16045a0370bcc7497d0216d94b0
Waverley Leung <waverley.nyc@gmail.com>
2020-06-07 22:40:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,3 +8,11 @@ function forLoop(array) {
   }
   return array
 }
+
+function whileLoop(number) {
+  let countdown = number
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  return "done"
+}
