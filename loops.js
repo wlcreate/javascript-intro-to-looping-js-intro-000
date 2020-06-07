4cdc0098c0c31683558863fefe7345d5c971e6fb
Waverley Leung <waverley.nyc@gmail.com>
2020-06-07 22:46:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,6 +17,12 @@ function whileLoop(number) {
   return "done"
 }
 
+let i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(integer) {
   do {
     console.log("I run once regardless.")
