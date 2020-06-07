abb7f4d50681ec44acaaf5f9ea291e7043511d89
Waverley Leung <waverley.nyc@gmail.com>
2020-06-07 22:31:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,14 +8,3 @@ function forLoop(array) {
   }
   return array
 }
-
-function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
-  }
-  return array;
-}
